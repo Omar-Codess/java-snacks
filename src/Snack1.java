b1f402efb241ee1ead7be32d7e91d03d2bc13088
Omar-Codess <omartaki8@gmail.com>
2023-03-09 14:21:07 +0100

    Snack 1

---
+++ src/Snack1.java
@@ -0,0 +1,19 @@
+//Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+import java.util.Scanner;
+
+public class Snack1 {
+    public static void main (String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Inserisci un numero:");
+        int userNum = Integer.parseInt(scan.nextLine());
+
+        if (userNum % 2 == 0){
+            System.out.print(userNum);
+        } else {
+            userNum += 1;
+            System.out.print(userNum);
+        }
+    }
+}
