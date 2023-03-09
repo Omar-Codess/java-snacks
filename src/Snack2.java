68cf318cd66984700425dbf7468c0e36111a2e77
Omar-Codess <omartaki8@gmail.com>
2023-03-09 17:12:28 +0100

    fixed random function

--- src/Snack2.java
+++ src/Snack2.java
@@ -1,14 +1,24 @@
 //Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi,
 //e da queste vuole mostrare a video una falsa lista di invitati con nome e cognome.
+import java.util.*;
 public class Snack2 {
     public static void main (String[] args){
-        String[] names = {"Mario ", "Antonio ", "Riccardo ", "Fausto "};
-        String[] surnames = {"Rossi", "Bianchi", "Verdi", "Neri"};
+        String[] names = {"Mario ", "Antonio ", "Martina ", "Riccardo ", "Fausto ", "Angelo ", "Domenico "};
+        String[] surnames = {"Rossi", "Bianchi","Chioschi ", "Verdi", "Neri", "Violi", "Azzurri"};
 
         System.out.println("Lista falsa partecipanti:");
+        Random ran = new Random();
+        int i = 0;
+        int p = 0;
 
-        for (int i = 0; i < names.length; i++){
-            System.out.println(names[i] + surnames[i]);
+        while(i < names.length || p < surnames.length){
+            i = ran.nextInt(names.length);
+            p = ran.nextInt(surnames.length);
+            System.out.println(names[i] + surnames[p]);
+            i++;
+            p++;
+            }
         }
     }
-}
+
+
